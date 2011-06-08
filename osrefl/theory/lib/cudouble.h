58b21cd10ccbae7946d6814da90a5d67f773a0be
mettingc <mettingc@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-06-08 13:13:41 +0000

    fixes relative import

--- osrefl/theory/lib/cudouble.h
+++ osrefl/theory/lib/cudouble.h
@@ -8,6 +8,6 @@ typedef double Real;
 const double eps = 2.22044604925e-16;
 const double RealMax = 1.79769313486+308;
 
-#include <_cucomplex.h>
+#include <lib/_cucomplex.h>
 
 #endif /* _CUDOUBLE_H_ */
