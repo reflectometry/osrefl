57d29f3d2cdd662904e90bfe64511f89df084652
Brian Maranville <brian.maranville@nist.gov>
2013-07-01 17:20:13 -0400

    properly dividing rho by q inside material (giving phase shift) but then still have to divide by q0 squared, which gives a term like q to the 4.

--- osrefl/theory/BAGISANS.py
+++ osrefl/theory/BAGISANS.py
@@ -16,8 +16,7 @@ class bornWavefunction:
         self.SLDArray = SLDArray
         self.r = zeros(kzlen, dtype=complex)
         self.r = self.calc_r(self.kz)
-        
-    
+           
     def calc_r(self, kz):
         qz = kz*2.0
         sld = self.SLDArray
@@ -36,11 +35,13 @@ class bornWavefunction:
         qz = kz*2.0
         sld = self.SLDArray
         rho = sld[:,0]
-        qz_l_sq = 4.0*((kz**2) - 4*pi*rho[:,None])
+        #qz_l_sq = 4.0*((kz**2) - 4*pi*rho[:,None])
+        qz_l = sqrt(4.0*((kz**2) - 4*pi*rho[:,None]))
         #print "qz_l_sq shape:", qz_l_sq.shape
         r = zeros_like(kz)
         zs = cumsum(sld, axis=0)
-        qzs = sqrt(qz_l_sq) * (sld[:,1,None])
+        #qzs = sqrt(qz_l_sq) * (sld[:,1,None])
+        qzs = qz_l * (sld[:,1,None])
         qzs = cumsum(qzs, axis=0)
         for i in range(len(sld)-1):
             for j in range(len(sld)-1):
@@ -48,10 +49,12 @@ class bornWavefunction:
                 qzj = qzs[j]
                 zi = zs[i][1] # interface i location
                 zj = zs[j][1]
-                dsldi = sld[i+1][0]/qz_l_sq[i+1] - sld[i][0]/qz_l_sq[i]
-                dsldj = sld[j+1][0]/qz_l_sq[j+1] - sld[j][0]/qz_l_sq[j]
+                #dsldi = sld[i+1][0]/qz_l_sq[i+1] - sld[i][0]/qz_l_sq[i]
+                #dsldj = sld[j+1][0]/qz_l_sq[j+1] - sld[j][0]/qz_l_sq[j]
+                dsldi = sld[i+1][0]/qz_l[i+1] - sld[i][0]/qz_l[i]
+                dsldj = sld[j+1][0]/qz_l[j+1] - sld[j][0]/qz_l[j]
                 #print dsldi.shape, qzj.shape, zj.shape
                 #print i,j,qzi-qzj, qz*(zi-zj)
-                r += 16*pi**2*dsldi*dsldj*cos(qzj-qzi)*exp(-(zj-zi)**2/(4.0*self.sigmaz**2))
+                r += 16*pi**2/qz**2*dsldi*dsldj*cos(qzj-qzi)*exp(-(zj-zi)**2/(4.0*self.sigmaz**2))
         return sqrt(r)
                 
