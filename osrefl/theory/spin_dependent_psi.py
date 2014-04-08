a8d205cd5e60e82292648110b8fd63c22cc83428
Brian Maranville <brian.maranville@nist.gov>
2014-04-08 18:41:42 -0400

    theory calculation of B is working now

--- osrefl/theory/spin_dependent_psi.py
+++ osrefl/theory/spin_dependent_psi.py
@@ -36,9 +36,10 @@ def calculateRB(kz, dz, rhoN, rhoM, mx, my, mz, AGUIDE):
         return [YA, YB, YC, YD];
     
     
-    B = zeros((N,4,4)) # one matrix per layer
+    global B 
+    B = zeros((N,4,4), dtype='complex') # one matrix per layer
     
-    newB = array([[1, 0, 0, 0], 
+    newB = matrix([[1, 0, 0, 0], 
                   [0, 1, 0, 0], 
                   [0, 0, 1, 0], 
                   [0, 0, 0, 1]], dtype='complex')
@@ -49,10 +50,12 @@ def calculateRB(kz, dz, rhoN, rhoM, mx, my, mz, AGUIDE):
     m_ip = sqrt(mx**2 + my**2) # in-plane projection of m unit vector
     m_ip_nz = (m_ip != 0.0) # mask for nonzero
     
-    mx_ip = zeros_like(mx) 
-    my_ip = zeros_like(my) 
-    mx_ip[m_ip_nz] = mx[m_ip_nz] / m_ip[m_ip_nz]
-    my_ip[m_ip_nz] = my[m_ip_nz] / m_ip[m_ip_nz]
+    #mx_ip = zeros_like(mx) 
+    #my_ip = zeros_like(my) 
+    #mx_ip[m_ip_nz] = mx[m_ip_nz] / m_ip[m_ip_nz]
+    #my_ip[m_ip_nz] = my[m_ip_nz] / m_ip[m_ip_nz]
+    
+    thetaM = arctan2(my, mx)
     
     rhoM_ip = zeros_like(rhoM)
     rhoM_ip[m_tot_nz] = rhoM[m_tot_nz] * m_ip[m_tot_nz] / m_tot[m_tot_nz]
@@ -61,16 +64,52 @@ def calculateRB(kz, dz, rhoN, rhoM, mx, my, mz, AGUIDE):
     
     S1 = sqrt(PI4*(rhoN + rhoM_ip)-KSQREL)
     S3 = sqrt(PI4*(rhoN - rhoM_ip)-KSQREL)
-    mu = (complex(1.0)  + mx_ip + 1j * my_ip) / (complex(1.0)  + mx_ip - 1j * my_ip)
-    
-    #mu_1 = (complex(1.0)  + mx + 1j * mz - my) / (complex(1.0)  + mx - 1j * mz + my)
-    #mu_3 = (complex(-1.0) + mx + 1j * mz - my) / (complex(-1.0) + mx - 1j * mz + my)
+    mu = exp(1j * thetaM)
     
     l=0
     step = 1
     z = complex(0)
     
-    for I in range(N-1):
+    if N>1:
+        # chi in layer l
+        lp = l+step
+        chi_l = matrix(\
+            [[  1,      1,      0,      0   ],
+             [  0,      0,      1,      1   ],
+             [S1[l], -S1[l],    0,      0   ],
+             [  0,      0,    S3[l], -S3[l] ]])
+        
+        # inverse chi at layer l+1
+        chi_inv_lp = matrix(\
+            [[ 1,  1/mu[lp],  1/S1[lp],  1/(mu[lp]*S1[lp]) ],
+             [ 1,  1/mu[lp], -1/S1[lp], -1/(mu[lp]*S1[lp]) ],
+             [ 1, -1/mu[lp],  1/S3[lp], -1/(mu[lp]*S3[lp]) ],
+             [ 1, -1/mu[lp], -1/S3[lp],  1/(mu[lp]*S3[lp]) ]])
+        chi_inv_lp *= 0.25
+        
+        S_l_vec = array([exp( S1[l] * z),
+                         exp(-S1[l] * z),
+                         exp( S3[l] * z),
+                         exp(-S3[l] * z)])
+                          
+        S_lp_inv_vec = array([exp(-S1[lp] * z),
+                              exp( S1[lp] * z),
+                              exp(-S3[lp] * z),
+                              exp( S3[lp] * z)])
+                                                       
+        S_lp_inv = matrix(eye(4,4) * S_lp_inv_vec)
+        S_l = matrix(eye(4,4) * S_l_vec)
+        
+        A = S_lp_inv * chi_inv_lp * chi_l * S_l
+        #print A
+        
+        newB = A * newB
+        #print newB
+        B[l] = newB.copy()
+        z += dz[l]
+        l = lp
+    
+    for I in range(1, N-1):
         # chi in layer l
         lp = l+step
         chi_l = matrix(\
@@ -87,34 +126,44 @@ def calculateRB(kz, dz, rhoN, rhoM, mx, my, mz, AGUIDE):
              [ 1, -1/mu[lp], -1/S3[lp],  1/(mu[lp]*S3[lp]) ]])
         chi_inv_lp *= 0.25
         
-        S_l_vec = matrix([[exp( 1j * S1[l] * z),
-                           exp(-1j * S1[l] * z),
-                           exp( 1j * S3[l] * z),
-                           exp(-1j * S3[l] * z)]])
+        S_l_vec = array([exp( S1[l] * z),
+                         exp(-S1[l] * z),
+                         exp( S3[l] * z),
+                         exp(-S3[l] * z)])
                           
-        S_lp_inv_vec = matrix([[exp(-1j * S1[lp] * z)],
-                               [exp( 1j * S1[lp] * z)],
-                               [exp(-1j * S3[lp] * z)],
-                               [exp( 1j * S3[lp] * z)]])                         
+        S_lp_inv_vec = array([exp(-S1[lp] * z),
+                              exp( S1[lp] * z),
+                              exp(-S3[lp] * z),
+                              exp( S3[lp] * z)])
+                                                       
+        S_lp_inv = matrix(eye(4,4) * S_lp_inv_vec)
+        S_l = matrix(eye(4,4) * S_l_vec)
         
-        A = S_lp_inv_vec * chi_inv_lp * chi_l * S_l_vec
+        A = S_lp_inv * chi_inv_lp * chi_l * S_l
+        #print A
         
         newB = A * newB
-        B[I] = newB.copy()
+        #print newB
+        B[l] = newB.copy()
         z += dz[l]
+        l = lp
     
-    newB = unitary_LAB_SAM_LAB(newB, AGUIDE)
+    denom = complex(1.0) / ((newB[3,3] * newB[1,1]) - (newB[1,3] * newB[3,1]))
+    YA_sam = ((newB[1,3] * newB[3,0]) - (newB[1,0] * newB[3,3])) * denom # r++
+    YB_sam = ((newB[1,0] * newB[3,1]) - (newB[3,0] * newB[1,1])) * denom # r+-
+    YC_sam = ((newB[1,3] * newB[3,2]) - (newB[1,2] * newB[3,3])) * denom # r-+
+    YD_sam = ((newB[1,2] * newB[3,1]) - (newB[3,2] * newB[1,1])) * denom # r--
     
-    denom = complex(1.0) / ((newB[3][3] * newB[1][1]) - (newB[1][3] * newB[3][1]))
-    YA_sam = ((newB[1][3] * newB[3][0]) - (newB[1][0] * newB[3][3])) * denom # r++
-    YB_sam = ((newB[1][0] * newB[3][1]) - (newB[3][0] * newB[1][1])) * denom # r+-
-    YC_sam = ((newB[1][3] * newB[3][2]) - (newB[1][2] * newB[3][3])) * denom # r-+
-    YD_sam = ((newB[1][2] * newB[3][1]) - (newB[3][2] * newB[1][1])) * denom # r--
-            
-    return [YA_sam, YB_sam, YC_sam, YD_sam];
+    r_lab = unitary_LAB_SAM_LAB2(matrix([[YA_sam, YB_sam], [YC_sam, YD_sam]]), AGUIDE);
+      
+    YA_lab = r_lab[0,0];
+    YB_lab = r_lab[0,1];
+    YC_lab = r_lab[1,0];
+    YD_lab = r_lab[1,1];
     
+    return YA_lab, YB_lab, YC_lab, YD_lab, B
     
-def get_U_sam_lab(AGUIDE): {
+def get_U_sam_lab(AGUIDE):
     C = complex(cos(AGUIDE/2.0*pi/180.))
     IS = 1j * sin(AGUIDE/2.0*pi/180.)
     U = matrix([ [C , IS, 0 , 0 ],
@@ -131,11 +180,90 @@ def get_Uinv_sam_lab(AGUIDE):
                     [0 , 0 , C , NS],
                     [0 , 0 , NS, C ] ])
     return Uinv
+    
+def get_U_sam_lab2(AGUIDE):
+    C = complex(cos(AGUIDE/2.0*pi/180.))
+    IS = 1j * sin(AGUIDE/2.0*pi/180.)
+    U = matrix([ [C , IS],
+                 [IS, C ] ])
+    return U
+
+def get_Uinv_sam_lab2(AGUIDE):
+    C = complex(cos(AGUIDE/2.0*pi/180.))
+    NS = 1j * -sin(AGUIDE/2.0*pi/180.)
+    Uinv = matrix([ [C , NS],
+                    [NS, C ] ])
+    return Uinv
 
 def unitary_LAB_SAM_LAB(A, AGUIDE):
     """ perform rotation of coordinate system on one side of matrix
     and perform inverse on the other side """
     U = get_U_sam_lab(AGUIDE)
-    Uinv = get_U_sam_lab(AGUIDE)
+    Uinv = get_Uinv_sam_lab(AGUIDE)
     CST =  (U * A) * Uinv
     return CST
+
+def unitary_LAB_SAM_LAB2(A, AGUIDE):
+    """ perform rotation of coordinate system on one side of matrix
+    and perform inverse on the other side """
+    U = get_U_sam_lab2(AGUIDE)
+    Uinv = get_Uinv_sam_lab2(AGUIDE)
+    CST =  (U * A) * Uinv
+    return CST
+
+def _test():
+    rhoN_mult = 2e-4
+    rhoM_mult = 2e-4
+    dz_mult = 5
+    rhoN = array([0.0, 2.0, 1.0,  2.0, 1.0, 2.0, 1.0, 0.0]) * rhoN_mult
+    rhoM = array([0.0, 1.0, 0.0,  1.0, 0.0, 1.0, 0.0, 0.0]) * rhoM_mult
+    my =   array([0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0, 0.0])
+    mx =   array([0.0, 0.0, 0.0,  0.0, 0.0, 0.0, 0.0, 0.0])
+    mz =   array([0.0, 0.0, 0.0,  0.0, 0.0, 0.0, 0.0, 0.0])
+    dz =   array([1.0, 1.0, 1.0,  1.0, 1.0, 1.0, 1.0, 1.0]) * dz_mult
+    AGUIDE = 0.0 # 90.0 would be along y
+    kz_array = linspace(0.01, 1.5, 201)
+    rpp = []
+    rpm = []
+    rmp = []
+    rmm = []
+    B = []
+    for kzi in kz_array:
+        result = list(calculateRB(kzi, dz, rhoN, rhoM, mx, my, mz, AGUIDE))
+        rpp.append(result[0])
+        rpm.append(result[1])
+        rmp.append(result[2])
+        rmm.append(result[3])
+        B.append(result[4])
+        
+    rpp = array(rpp)
+    rpm = array(rpm)
+    rmp = array(rmp)
+    rmm = array(rmm)
+    B = array(B)
+    
+    from pylab import *
+    plot(2*kz_array, abs(rpp)**2, label="r++")
+    plot(2*kz_array, abs(rmp)**2, label="r-+")
+    plot(2*kz_array, abs(rpm)**2, label="r+-")
+    plot(2*kz_array, abs(rmm)**2, label="r--")
+    legend()
+    show()
+    
+    return rpp, rpm, rmp, rmm, B
+
+def multiply4x4(A, B):
+    C = matrix( [[0,0,0,0],
+                 [0,0,0,0],
+                 [0,0,0,0],
+                 [0,0,0,0]], dtype='complex' )
+    for i in range(4):
+        for j in range(4):
+            for k in range(4):
+                C[i,j] += (A[i,k] * B[k,j])
+    return C
+
+if __name__ == '__main__': 
+    rpp, rpm, rmp, rmm, B = _test()
+    
+    
